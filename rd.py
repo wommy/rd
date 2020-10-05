c2938987180c1939d9ea60f157258579eb810ed8
wommy <tom@technomad.media>
2020-10-05 14:20:14 -0400

    pushit

--- rd.py
+++ rd.py
@@ -7,13 +7,11 @@ import datetime
 # new time
 time = datetime.datetime.now()
 
-# for quarter
-q = str( int( int( time.strftime( "%W" ) ) / 13 + 1 ) )
 # for week in quarter
 wm = str( int( int( time.strftime( "%W" ) ) % 13 + 1 ) )
 
 # format for filename - YYq#-MMw#
-filenameTime = time.strftime('%yq')+q+time.strftime('-%mw')+wm
+filenameTime = time.strftime('%mw')+wm
 
 # new file name w/ format: filenameTime-title.md
 filename = filenameTime+'-'+topic+'.md'
